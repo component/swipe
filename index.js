cba42c73fe07ddf0f50653adabcac87392c6f3d0
Guillermo Rauch <rauchg@gmail.com>
2013-06-01 15:40:57 -0700

    index: make `refresh` not emit events

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ Swipe.prototype.refresh = function(){
   this.width = Math.ceil(this.childWidth * visible);
   this.child.style.width = this.width + 'px';
   this.child.style.height = this.height + 'px';
-  this.show(this.currentVisible, 0);
+  this.show(this.currentVisible, 0, { silent: false });
 };
 
 /**
