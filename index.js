624063515ec2aa9b16ecedba9e8d3694f3ed815a
Nathan Rajlich <nathan@tootallnate.net>
2013-11-12 16:02:50 -0800

    use component/indexof

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var style = require('computed-style');
 var Emitter = require('emitter');
 var event = require('event');
 var events = require('events');
+var indexOf = require('indexof');
 var min = Math.min;
 var max = Math.max;
 
@@ -463,22 +464,6 @@ Swipe.prototype.translate = function(x){
     s[transform] = 'translateX(' + x + 'px)';
   }
 };
-
-/**
- * Return index of `el` in `els`.
- *
- * @param {Array} els
- * @param {Element} el
- * @return {Number}
- * @api private
- */
-
-function indexOf(els, el) {
-  for (var i = 0; i < els.length; i++) {
-    if (els[i] == el) return i;
-  }
-  return -1;
-}
 
 /**
  * Check if `el` is visible.
